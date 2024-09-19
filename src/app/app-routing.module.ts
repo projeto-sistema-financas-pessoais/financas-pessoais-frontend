a0bb5366491c92a182bf66ec0e608a0bf10cc65d
Lais Portela <lais3722@gmail.com>
2024-09-19 13:22:51 -0300

    feat: add sidebar, logo, and colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   {
     path: 'inicio',
     canActivate: [authGuard],
+    data: {title: 'Início'},
     loadChildren: () =>
     import('./features/pages/landing-page/landing-page.module').then((x)=> x.LandingPageModule)
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { LandingPageComponent } from './features/pages/landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import { SidebarComponent } from './features/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     ModalModule.forRoot(),
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+
   ],
   exports: [
     HttpClientModule,
