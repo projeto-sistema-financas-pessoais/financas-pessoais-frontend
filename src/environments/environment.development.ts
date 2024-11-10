fc1f73a43436a46ee76aea0284fb2c02944d40a3
Lais Portela <lais3722@gmail.com>
2024-11-10 01:21:18 -0300

    update enviromment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    financas: 'http://localhost:9000/api/v1'
+    // financas: 'http://localhost:9000/api/v1'
+    financas: 'http://204.48.28.39/api/v1'
+
 };
