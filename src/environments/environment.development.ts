9d8e397595e46cb349c926237ceda6247158fd47
Lais Portela <lais3722@gmail.com>
2024-11-13 10:24:29 -0300

    refactor: update enviromment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    financas: 'http://localhost:9000/api/v1'
-    // financas: 'https://financas-pessoais-api.lat/api/v1'
+    // financas: 'http://localhost:9000/api/v1'
+    financas: 'https://financas-pessoais-api.lat/api/v1'
 
 };
